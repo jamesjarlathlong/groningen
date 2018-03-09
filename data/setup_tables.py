dc262be018d1d68845682ab75776af0abffc4af0
james <jjlong@mit.edu>
2018-03-09 18:44:33 -0500

    finished initial data prep

--- data/setup_tables.py
+++ data/setup_tables.py
@@ -1,26 +1,28 @@
-import helpers
+import query_helpers
 import sqlite3
 import functools
 import pandas as pd
 import numpy as np
 import json
 dbname = 'groningendata.db'
-sqlconn = functools.partial(helpers.with_connection, sqlite3,dbname)
+sqlconn = functools.partial(query_helpers.with_connection, sqlite3,dbname)
 
 @sqlconn
 def setup_events(cnn):
 	url = 'http://rdsa.knmi.nl/fdsnws/event/1/query?format=text&nodata=404'
 	df = pd.read_csv(url,sep='|')
 	df.columns=df.columns.str.replace('#','')
-	helpers.df_to_sqlite(cnn,df,'events')
+	query_helpers.df_to_sqlite(cnn,df,'events')
 	df.to_csv('events.csv', index=False)
+	cnn.execute('''CREATE INDEX eventsidx ON events (eventid)''')
 @sqlconn
 def setup_stations(cnn):
 	url = 'http://rdsa.knmi.nl/fdsnws/station/1/query?format=text&nodata=404'
 	df = pd.read_csv(url,sep='|')
 	df.columns=df.columns.str.replace('#','')
-	helpers.df_to_sqlite(cnn,df,'stations')
+	query_helpers.df_to_sqlite(cnn,df,'stations')
 	df.to_csv('stations.csv', index=False)
+	cnn.execute('''CREATE INDEX stationssidx ON stations (station)''')
 @sqlconn
 def setup_groundmotion(cnn):
 	cur = cnn.cursor()
@@ -31,7 +33,7 @@ def setup_groundmotion(cnn):
 		 FOREIGN KEY (eventid) REFERENCES events (eventid),
 		 FOREIGN KEY (stationid) REFERENCES stations (station)
 		 )''')
-	cur.execute('''CREATE INDEX eventsidx ON groundmotion (eventid)''')
+	cur.execute('''CREATE INDEX groundeventsidx ON groundmotion (eventid)''')
 if __name__=='__main__':
 	setup_stations()
 	setup_events()
